9085b660321e057a5755d17963a6b861dd07f4dc
Adnan Hadzialic <adnan.hadzialic@outlook.com>
2023-04-08 19:28:39 +0200

    DatabaseContext class with connection to local database.

---
+++ BlogPlatform.API/Data/DatabaseContext.cs
@@ -0,0 +1,23 @@
+using BlogPlatform.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogPlatform.API.Data;
+
+public class DatabaseContext:DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DatabaseContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to postgres with connection string from app settings
+        options.UseNpgsql(Configuration.GetConnectionString("LocalConnection"));
+    }
+    public DbSet<Blog> Blogs { get; set; }
+
+    public DbSet<Comment> Comments { get; set; }
+
+}
