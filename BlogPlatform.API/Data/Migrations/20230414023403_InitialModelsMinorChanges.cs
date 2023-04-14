51726de03a0586526024116d2de6432ffa28df76
Adnan Hadzialic <adnan.hadzialic@outlook.com>
2023-04-14 04:38:07 +0200

    Migration for minor model changes.

---
+++ BlogPlatform.API/Data/Migrations/20230414023403_InitialModelsMinorChanges.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogPlatform.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialModelsMinorChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
